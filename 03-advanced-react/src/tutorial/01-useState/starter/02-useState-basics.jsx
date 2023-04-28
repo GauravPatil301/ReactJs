40f9b06e5ed1137f7a9db73f5480f0861e1e2b27
Gaurav Patil <301gaurav@gmail.com>
2023-04-28 19:12:57 +0530

    useState-basic completed

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  console.log(useState(1));
-  return <h2>useState basics</h2>;
+  // console.log(useState(1));
+  // const value = useState('hello')[0];
+  // const func = useState('hello')[1];
+  // console.log(value);
+  // console.log(func);
+
+  const [count, setCount] = useState(0);
+  const [name, setName] = React.useState()
+
+  const handleClick = () => {
+    setCount(count + 1)
+    // setCount('pants')
+  }
+
+  return (
+  <div>
+    <h4>You Clicked {count}</h4>
+    <button type="button" className="btn" onClick={handleClick}>Click Me!</button>
+  </div>
+  );
 };
 
 export default UseStateBasics;
