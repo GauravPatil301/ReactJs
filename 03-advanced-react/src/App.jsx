5b78c6f02a2639a1c9eec607bf3947d5b1424f12
Gaurav Patil <301gaurav@gmail.com>
2023-05-02 09:11:01 +0530

    04 project structure module done

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,15 @@
-import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
+// import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
+// import Home from './tutorial/04-project-structure/starter/Pages/Home'
 
+import Example from './tutorial/04-project-structure/starter/Example'
+
+// import { Home, About } from './tutorial/04-project-structure/starter/Pages'
 function App() {
   return (
-    <div className='container'>
-      <Starter />
+    <div className="container">
+      <Example />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  console.log('render');
+  return (
+  <div>
+    
+    <button className="btn" onClick={() => setToggle(!toggle)}>toggle component</button>
+    {toggle && <RandomComponent/>}
+  </div>);
 };
+
+const RandomComponent = () => {
+  useEffect(() => {
+    // console.log('hmm,This is interesting');
+    // const intId = setInterval(() => {
+    //   console.log('hello from interval');
+    // }, 1000);
+    // return() =>{
+    //   clearInterval(intId);
+    //   // console.log('cleanup');
+    // }
+
+    const someFunc = () => {
+      // logic
+    };
+    window.addEventListener('scroll',someFunc);
+    return () => window.removeEventListener('scroll',someFunc)
+  },[]);
+  return <h1>hello there</h1>;
+}
 
 export default CleanupFunction;
