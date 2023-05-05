c566e642d239f51606e036999d9af54ee5c30d9e
Gaurav Patil <301gaurav@gmail.com>
2023-05-05 12:51:41 +0530

    11-performance done

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,9 @@
-import Starter from './tutorial/10-useReducer/starter/01-useReducer'
+import Starter from './tutorial/11-performance/starter/04-react-18'
 function App() {
   return (
-    <>
+    <div className='container'>
       <Starter/>
-    </>
+    </div>
   )
 }
 
--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useState, useCallback,useMemo } from "react";
+import { data } from "../../../../data";
+import List from "./List";
+import slowFunction from "./slowFunction";
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
-  const removePerson = (id) => {
+  const value = useMemo(()=> slowFunction(),[])
+  
+  console.log(value);
+
+  const removePerson = useCallback((id) => {
+    console.log(people);  
     const newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople)
-  }
+    setPeople(newPeople);
+  }, [people]);
   return (
     <section>
       <button
-        className='btn'
+        className="btn"
         onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
+        style={{ marginBottom: "1rem" }}
       >
         count {count}
       </button>
-      <List people={people} removePerson={removePerson}/>
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
